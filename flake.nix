7078ae4815b9d2c198d4238ec82cd537e2bde4df
Vinícius Daniel <dannful@gmail.com>
2024-12-03 19:18:07 -0300

    Updated nix flake file

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A flake for a project with Make, GCC, Bison, and Flex";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.gcc pkgs.gnumake pkgs.bison pkgs.flex ];
+        };
+      in { devShell = devShell; });
+}
