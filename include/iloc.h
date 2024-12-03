636e1580b3e80182a52619d51351134945c2e218
Vinícius Daniel <dannful@gmail.com>
2024-12-03 15:52:56 -0300

    Improved ILOC struct

--- include/iloc.h
+++ include/iloc.h
@@ -2,12 +2,26 @@
 
 #include <stdint.h>
 
-#define SOURCE_REGISTERS 2
-#define SOURCE_OPERANDS 2
+typedef struct {
+  uint8_t register;
+  int operand;
+} iloc_register_operand_t;
+
+typedef union {
+  uint8_t source_registers[2];
+  uint8_t source_register;
+  iloc_register_operand_t register_operand;
+} iloc_instruction_data_t;
+
+typedef enum {
+  REGISTER_REGISTER_DEST = 0,
+  REGISTER_OPERAND_DEST = 1,
+  REGISTER_DEST_OPERAND = 2,
+  REGISTER_DEST = 3
+} iloc_instruction_type_t;
 
 typedef struct {
   const char *mnemonic;
-  uint8_t source_registers[SOURCE_REGISTERS];
-  int operands[SOURCE_OPERANDS];
-  uint8_t destination_register;
+  iloc_instruction_type type;
+  iloc_instruction_data_t data;
 } iloc_instruction_t;
