6300570d9eff08f6111fbbe1d99707abf33a81ee
Vinícius Daniel <dannful@gmail.com>
2024-12-10 19:39:15 -0300

    Fixed nix file

--- flake.nix
+++ flake.nix
@@ -1,25 +1,23 @@
 {
-  description = "A flake for a project with Make, GCC, Bison, and Flex";
+  description = "Nix flake file for a compiler project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        stdenv = pkgs.gcc14Stdenv;
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.gcc
-            pkgs.gnumake
-            pkgs.bison
-            pkgs.flex
-            pkgs.clang
-            pkgs.python3
-          ];
+        devShells.default = pkgs.mkShell.override { inherit stdenv; } {
+          buildInputs = with pkgs; [ flex bison ];
+          packages = with pkgs; [ llvmPackages_17.clang-tools ];
+          env = {
+            CLANGD_FLAGS = "--query-driver=${pkgs.lib.getExe stdenv.cc}";
+          };
         };
       });
 }
